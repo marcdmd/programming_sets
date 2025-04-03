d5a197bd7ea6310da3bd031810a7c5e4e916a72f
marcdmd <marc.dungo@student.ateneo.edu>
2025-04-03 11:52:03 +0800

    Create set_3.py

---
+++ set_3.py
@@ -0,0 +1,47 @@
+def relationship_status(from_member, to_member, social_graph):
+    '''Relationship Status.
+
+    Let us pretend that you are building a new app.
+    Your app supports social media functionality, which means that users can have
+    relationships with other users.
+
+    There are two guidelines for describing relationships on this social media app:
+    1. Any user can follow any other user.
+    2. If two users follow each other, they are considered friends.
+
+    This function describes the relationship that two users have with each other.
+
+    Please see "assignment-4-sample-data.py" for sample data. The social graph
+    will adhere to the same pattern.
+
+    Parameters
+    ----------
+    from_member: str
+        the subject member
+    to_member: str
+        the object member
+    social_graph: dict
+        the relationship data
+
+    Returns
+    -------
+    str
+        "follower" if fromMember follows toMember,
+        "followed by" if fromMember is followed by toMember,
+        "friends" if fromMember and toMember follow each other,
+        "no relationship" if neither fromMember nor toMember follow each other.
+    '''
+    from_following = social_graph.get(from_member, {}).get("following", [])
+    to_following = social_graph.get(to_member, {}).get("following", [])
+
+    from_follows_to = to_member in from_following
+    to_follows_from = from_member in to_following
+
+    if from_follows_to and to_follows_from:
+        return "friends"
+    elif from_follows_to:
+        return "follower"
+    elif to_follows_from:
+        return "followed by"
+    else:
+        return "no relationship"
